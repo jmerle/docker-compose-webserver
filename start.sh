d18150f07ca3294a4997b2d2939f673076781ed3
Jasper van Merle <jaspervmerle@gmail.com>
2018-11-21 00:29:31 +0100

    Change to DNS challenge for Let's Encrypt with wildcard support

--- start.sh
+++ start.sh
@@ -14,6 +14,18 @@ if [ ! -e acme.json ]; then
   chmod 600 acme.json
 fi
 
+# Create the acme.domains arguments
+for i in ${!ALL_DOMAINS[@]}; do
+  domain="${ALL_DOMAINS[$i]}"
+
+  domains="--acme.domains='*.$domain,$domain'"
+  if [ "$i" -eq "0" ]; then
+    export TRAEFIK_DOMAINS="$domains"
+  else
+    export TRAEFIK_DOMAINS="$TRAEFIK_DOMAINS $domains"
+  fi
+done
+
 # Create the network if it does not exist yet
 docker network inspect $TRAEFIK_NETWORK &>/dev/null || docker network create $TRAEFIK_NETWORK
 
