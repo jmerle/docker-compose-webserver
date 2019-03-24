cf4b1078bc572652f330bab79f3c93cf9fb6caa5
Jasper van Merle <jaspervmerle@gmail.com>
2019-03-24 14:06:08 +0100

    Replace DNS challenge with HTTP challenge

--- start.sh
+++ start.sh
@@ -8,29 +8,11 @@ else
   exit 1
 fi
 
-# Check if the private TransIP key file exists
-if [ ! -e transip.key ]; then
-  echo "Please create a transip.key file containing your private TransIP key which is used when creating SSL certificates."
-  exit 1
-fi
-
 # Create acme.json file
 if [ ! -e acme.json ]; then
   touch acme.json
   chmod 600 acme.json
 fi
-
-# Create the acme.domains arguments
-for i in ${!ALL_DOMAINS[@]}; do
-  domain="${ALL_DOMAINS[$i]}"
-
-  domains="--acme.domains='*.$domain,$domain'"
-  if [ "$i" -eq "0" ]; then
-    export TRAEFIK_DOMAINS="$domains"
-  else
-    export TRAEFIK_DOMAINS="$TRAEFIK_DOMAINS $domains"
-  fi
-done
 
 # Create the network if it does not exist yet
 docker network inspect $TRAEFIK_NETWORK &>/dev/null || docker network create $TRAEFIK_NETWORK
